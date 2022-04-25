a3ff9149c4e5f009558c9ee5a4d1f5645688ae7b
nabab <thomas.nabet@gmail.com>
2022-04-26 01:46:48 +0200

    Cleaned database

--- src/components/provider/costs/costs.php
+++ src/components/provider/costs/costs.php
@@ -11,64 +11,63 @@
     <bbns-column title="<?= _("Territory") ?>"
                  ftitle="<?= _("Continent or country") ?>"
                  field="territory"
-                 :source="destinations">
-    </bbns-column>
+                 :source="destinations"/>
     <bbns-column title="<?= _("< 500g") ?>"
                  ftitle="<?= _("Under 500g") ?>"
                  type="money"
                  currency="€"
+                 :precision="2"
                  :nullable="true"
                  width="5em"
-                 field="g500">
-    </bbns-column>
+                 field="g500"/>
     <bbns-column title="<?= _("< 1kg") ?>"
                  ftitle="<?= _("Between 500g and 1kg") ?>"
                  type="money"
+                 :precision="2"
                  currency="€"
                  :nullable="true"
                  width="5em"
-                 field="g1000">
-    </bbns-column>
+                 field="g1000"/>
     <bbns-column title="<?= _("< 1.5kg") ?>"
                  ftitle="<?= _("Between 1kg and 1.5kg") ?>"
                  type="money"
+                 :precision="2"
                  currency="€"
                  :nullable="true"
                  width="5em"
-                 field="g1500">
-    </bbns-column>
+                 field="g1500"/>
     <bbns-column title="<?= _("< 2kg") ?>"
                  ftitle="<?= _("Between 1.5kg and 2kg") ?>"
                  type="money"
+                 :precision="2"
                  :nullable="true"
                  currency="€"
                  width="5em"
-                 field="g2000">
-    </bbns-column>
+                 field="g2000"/>
     <bbns-column title="<?= _("< 2.5kg") ?>"
                  ftitle="<?= _("Between 2kg and 2.5kg") ?>"
                  :nullable="true"
                  type="money"
+                 :precision="2"
                  currency="€"
                  width="5em"
-                 field="g2500">
-    </bbns-column>
+                 field="g2500"/>
     <bbns-column title="<?= _("< 3kg") ?>"
                  ftitle="<?= _("Between 2.5kg and 3kg") ?>"
+                 :precision="2"
                  type="money"
                  currency="€"
                  :nullable="true"
                  width="5em"
-                 field="g3000">
-    </bbns-column>
+                 field="g3000"/>
     <bbns-column title="<?= _("+ 1kg") ?>"
                  ftitle="<?= _("Every kg added") ?>"
+                 :precision="2"
                  type="money"
                  currency="€"
                  :nullable="true"
                  width="5em"
-                 field="gm1000">
-    </bbns-column>
+                 field="gm1000"/>
     <bbns-column title="<?= _(" ") ?>"
                  width="7em"
                  :buttons="[{
@@ -82,7 +81,6 @@
                            icon: 'nf nf-fa-times',
                            action: 'delete'
                            }]"
-                 field="gm1000">
-    </bbns-column>
+                 field="gm1000"/>
   </bbn-table>
 </div>
