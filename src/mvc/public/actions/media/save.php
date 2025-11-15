36ab675e60cdaf796b0b90346ce45a7f0c91fd2a
nabab <thomas.nabet@gmail.com>
2025-11-15 05:12:28 +0100

    Implement new Str functions: pos, rpos, replace, regplace, len, substr

--- src/mvc/public/actions/media/save.php
+++ src/mvc/public/actions/media/save.php
@@ -1,4 +1,5 @@
 <?php
+use bbn\Str;
 /*
  * Describe what it does!
  *
@@ -14,14 +15,14 @@ if (!empty($ctrl->post['media']['file']) && ($title = $ctrl->post['media']['titl
   $fs = new \bbn\File\System();
   $media = [];
   foreach($files as $f){
-    if(strpos($f['extension'], '.') > -1){
+    if(Str::pos($f['extension'], '.') > -1){
       $f['extension'] = str_replace('.', '', $f['extension']);
     }
     $_path = $tmp_path.$f['name'];
     if ( $fs->isFile($_path) ){
       $content = file_get_contents($_path);
       if (!empty($ctrl->post['media']['name']) && ( $ctrl->post['media']['name'] !== $f['name'])){
-        if ( strpos( $ctrl->post['media']['name'], '.') > -1 ){
+        if ( Str::pos( $ctrl->post['media']['name'], '.') > -1 ){
           //if the name contains the extension
           $name = $ctrl->post['media']['name'];
         }
@@ -32,7 +33,7 @@ if (!empty($ctrl->post['media']['file']) && ($title = $ctrl->post['media']['titl
       else{
         $name = $f['name'];
       }
-			if ( strpos( $title, '.') < 0 ){
+			if ( Str::pos( $title, '.') < 0 ){
         //if the title does not contains extension
         $title = $title.'.'.$f['extension'];
       }
